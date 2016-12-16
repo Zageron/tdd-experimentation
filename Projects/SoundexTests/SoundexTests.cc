3f034570e5e9f16c4dbda5400ecaf689b1b0b61a
Adam Bryant <adam.bryant@skyboxlabs.com>
2016-12-15 18:28:12 -0800

    Created a stubbed encoding class, passed a "single character encoding" test.

--- Projects/SoundexTests/SoundexTests.cc
+++ Projects/SoundexTests/SoundexTests.cc
@@ -1,12 +1,21 @@
-#include "gtest\gtest.h"
-#include "Soundex.h"
+#include "gmock\gmock.h"
+using ::testing::Eq;
+
 
 class Soundex
 {
-
+public:
+	std::string encode(const std::string& word) const
+	{
+		return word;
+	}
 };
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
 {
 	Soundex soundex;
+
+	auto encoded = soundex.encode("A");
+
+	ASSERT_THAT(encoded, Eq("A"));
 }
