383261325384e34945148e2db91002574c2f0f7d
Adam Bryant <adam.bryant@skyboxlabs.com>
2016-12-16 18:52:18 -0800

    2.12

--- Projects/SoundexTests/SoundexTests.cpp
+++ Projects/SoundexTests/SoundexTests.cpp
@@ -34,7 +34,13 @@ TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits)
 {
 	ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
 }
+
 TEST_F(SoundexEncoding, LimitsLengthToFourCharacters)
 {
 	ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
+}
+
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters)
+{
+	ASSERT_THAT(soundex.encode("Baeiouhycdl"), Eq("B234"));
 }
